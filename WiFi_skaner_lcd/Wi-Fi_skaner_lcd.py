1c2bdf4b3383c091d4fd4ad0fc4f442df4172583
avarte <avarte@mail.ru>
2026-03-23 19:07:23 +0600

    skaner_s_restart

--- WiFi_skaner_lcd/Wi-Fi_skaner_lcd.py
+++ WiFi_skaner_lcd/Wi-Fi_skaner_lcd.py
@@ -11,76 +11,76 @@ tft.init_7735(tft.GREENTAB80x160)
 tft.rotation(3)
 
 # --- Кнопки ---
-up_key   = Pin(13, Pin.IN, Pin.PULL_UP)
-down_key = Pin(8,  Pin.IN, Pin.PULL_UP)
+up_key     = Pin(13, Pin.IN, Pin.PULL_UP)
+down_key   = Pin(8,  Pin.IN, Pin.PULL_UP)
+center_key = Pin(4,  Pin.IN, Pin.PULL_UP) # Кнопка для рескана
 
 # --- Настройка WiFi ---
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
-tft.fill(TFT.BLACK)
-tft.text((30, 35), "SCANNING WIFI...", TFT.YELLOW, sysfont, 1)
-
-# Сканируем доступные сети
-raw_nets = wlan.scan()
+# Глобальные переменные данных
 nets = []
-for n in raw_nets:
-    try:
-        ssid = n[0].decode('utf-8')
-    except:
-        ssid = "<Hidden>"
-    rssi = n[3]
-    nets.append((ssid, rssi))
-
-# Сортировка по силе сигнала
-nets.sort(key=lambda x: x[1], reverse=True)
-
-# Параметры отображения
 scroll_pos = 0      
 visible_count = 6   
 line_h = 13         
-total_nets = len(nets)
+total_nets = 0
 needs_update = True
+
+def perform_scan():
+    global nets, total_nets, scroll_pos, needs_update
+    tft.fill(TFT.BLACK)
+    tft.text((30, 35), "SCANNING...", TFT.YELLOW, sysfont, 1)
+    
+    raw_nets = wlan.scan()
+    nets = []
+    for n in raw_nets:
+        try:
+            ssid = n[0].decode('utf-8')
+        except:
+            ssid = "<Hidden>"
+        rssi = n[3]
+        nets.append((ssid, rssi))
+    
+    # Сортировка по силе сигнала
+    nets.sort(key=lambda x: x[1], reverse=True)
+    total_nets = len(nets)
+    scroll_pos = 0 # Сброс прокрутки при новом поиске
+    needs_update = True
+    print(f"Found {total_nets} networks")
 
 def draw_interface():
     tft.fill(TFT.BLACK)
-    
     if total_nets == 0:
-        tft.text((20, 35), "NO NETWORKS FOUND", TFT.RED, sysfont, 1)
+        tft.text((20, 35), "NO NETWORKS", TFT.RED, sysfont, 1)
         return
 
-    # 1. Полоса прокрутки (исправлен цвет с кортежа на константу)
+    # 1. Полоса прокрутки
     bar_height = max(10, int((visible_count / total_nets) * 80))
     bar_y = int((scroll_pos / total_nets) * 80)
-    
-    # Фоновая линия (используем белый или серый, если он есть в библиотеке)
     tft.fillrect((0, 0), (3, 80), TFT.WHITE) 
-    # Ползунок (рисуем черным внутри белого или инверсно)
     tft.fillrect((0, bar_y), (3, bar_height), TFT.RED)
 
-    # 2. Список сетей (по 6 строк)
+    # 2. Список сетей
     for i in range(visible_count):
         idx = scroll_pos + i
         if idx < total_nets:
             ssid, rssi = nets[idx]
             y = i * line_h + 2
-            
-            # Цвет в зависимости от уровня сигнала
             color = TFT.GREEN if rssi > -60 else (TFT.YELLOW if rssi > -80 else TFT.RED)
-            
-            # Форматированный вывод
             txt = "{:d}.{:.12s}".format(idx+1, ssid)
             tft.text((6, y), txt, color, sysfont, 1)
             tft.text((125, y), str(rssi), TFT.WHITE, sysfont, 1)
 
-print(f"Found {total_nets} networks")
+# Первый запуск сканирования
+perform_scan()
 
 while True:
     if needs_update:
         draw_interface()
         needs_update = False
 
-    # Листаем ВВЕРХ на 6 строк
+    # Листаем ВВЕРХ (P13)
     if up_key.value() == 0:
         if scroll_pos > 0:
             scroll_pos -= visible_count
@@ -88,11 +88,17 @@ while True:
             needs_update = True
             utime.sleep_ms(250)
 
-    # Листаем ВНИЗ на 6 строк
+    # Листаем ВНИЗ (P8)
     if down_key.value() == 0:
         if scroll_pos + visible_count < total_nets:
             scroll_pos += visible_count
             needs_update = True
             utime.sleep_ms(250)
 
+    # РЕСКАН (P4)
+    if center_key.value() == 0:
+        perform_scan()
+        utime.sleep_ms(500)
+
     utime.sleep_ms(20)
+
